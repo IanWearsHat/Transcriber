340157e4a7f6297cd6098b0ff20534e9003eb6a9
Ian Dai <ianbbdai@gmail.com>
2023-08-11 21:25:28 -0700

    Created Watchdog class, can download pdfs from emails

--- watchdog.py
+++ watchdog.py
@@ -3,14 +3,18 @@ import keyboard
 import imaplib
 import email
 from email.header import decode_header
-import os
+import pathlib
+
+debug = False
 
 # Godaddy IMAP server: imap.secureserver.net
 IMAP_SERVER = "outlook.office365.com"
+EMAIL_FOLDER = "INBOX"
+
 
 """
-Username in first line
-Password in second line
+    Username in first line
+    Password in second line
 """
 def get_credentials():
     with open('credentials.txt', 'r') as f:
@@ -18,32 +22,71 @@ def get_credentials():
         return lines[0].strip(), lines[1].strip()
 
 
-def login():
-    usr, pwd = get_credentials()
+class Watchdog:
+    def __init__(self):
+        self.imap = self.login()
 
-    imap = imaplib.IMAP4_SSL(IMAP_SERVER)
-    imap.login(usr, pwd)
+    def login(self):
+        usr, pwd = get_credentials()
 
-    return imap
+        imap_obj = imaplib.IMAP4_SSL(IMAP_SERVER)
+        imap_obj.login(usr, pwd)
 
+        return imap_obj
 
-def check_for_emails(imap):
-    status, messages = imap.select("INBOX")
+    def check_for_attachments(self, msg):
+        if msg.is_multipart():
+            for part in msg.walk():
+                content_disposition = str(part.get("Content-Disposition"))
 
-    messages = int(messages[0])
-    print(status, messages)
+                if "attachment" in content_disposition:
+                    filename = part.get_filename()
+                    # only download if the attachment is a pdf and it doesn't already exist
+                    if ".pdf" in filename and not pathlib.Path(filename).is_file():
+                        self.download_attachment(part, filename)
+
+    def download_attachment(self, part, filename):
+        open(filename, "wb").write(part.get_payload(decode=True))
+
+    def fetch_all_emails(self, msg_num_list):
+        for msg_num in msg_num_list:
+            res, msg = self.imap.fetch(msg_num.decode(), "(RFC822)")  # fetch email by id
+            for response in msg:
+                if isinstance(response, tuple):
+                    msg = email.message_from_bytes(response[1])
+
+                    if debug:
+                        print('\n')
+                        print(msg["Message-ID"])
+                        print(msg["From"])
+                        print(msg["Subject"])
+
+                    self.check_for_attachments(msg)
+
+    def check_for_new_emails(self):
+        self.imap.select(EMAIL_FOLDER, readonly=True)  # readonly=True so that unread messages retain unread flag
+        status, messages = self.imap.search(None, "(UNSEEN)")
+
+        if status == "OK":
+
+            if debug:
+                print("All message IDs:\t", [num for num in messages[0].split()])
+
+            self.fetch_all_emails(messages[0].split())
+
+    def start(self):
+        self.check_for_new_emails()
 
 
 def main():
-    imap = login()
-    check_for_emails(imap)
+    Watchdog().start()
 
     # run = True
     # while run:
     #     if keyboard.is_pressed('p'):
     #         run = False
     #
-    #     check_for_emails(imap)
+    #     check_for_new_emails(imap)
     #
     #     time.sleep(0.02)
 
