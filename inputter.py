06ac7a723961de501276e1f57c711a7321f84426
Ian Dai <56898254+IanWearsHat@users.noreply.github.com>
2023-09-07 21:57:55 -0700

    Post-FreightStream test

--- inputter.py
+++ inputter.py
@@ -1,7 +1,8 @@
 import time
 import pyautogui as gui
 
-
+# rename mawb to just awb
+# also the thing should be clicking on hawb list, not mawb
 class Inputter:
     def __init__(self, data: dict):
         """
@@ -22,19 +23,40 @@ class Inputter:
             }
         ]
         """
-        self.vendor = data['vendor']
-        self.date = data['date']
-        self.invoice_num = data['invoice_num']
-        self.rows = data['rows']
+        # self.vendor = data['vendor']
+        # self.date = data['date']
+        # self.invoice_num = data['invoice_num']
+        # self.rows = data['rows']
+        self.vendor = ''
+        self.date = ''
+        self.invoice_num = ''
 
     def click_air_import(self):
         gui.click(x=415, y=45)
 
     def click_mawb_list(self):
-        gui.click(x=480, y=77)
+        gui.click(x=480, y=104)
 
     def search_mawb(self):
-        pass
+        # the invoice does not have a reference number
+        # it should be the mawb, so use that instead
+        # however, the other steps should be the same
+        
+        # click on reference number column
+        # press F5
+        # enter search term
+        # press enter
+        # top row will be the correct one
+        # then hit F7
+        time.sleep(4)
+        gui.click(x=513, y=165)
+        time.sleep(2)
+        gui.press('f5')
+        time.sleep(2)
+        gui.write('LAI23070046')
+        time.sleep(0.5)
+        gui.press('enter')
+        time.sleep(4)
 
     def click_account_payable_header(self):
         gui.click(x=645, y=590)
@@ -51,14 +73,11 @@ class Inputter:
         # input post
         gui.click(x=300, y=410)
         gui.write(self.date)
-
-        # input due
-        gui.click(x=300, y=400)
-        gui.write(self.date)
-
+        time.sleep(1)
         # input invoice date
         gui.click(x=470, y=410)
         gui.write(self.date)
+        time.sleep(1)
 
     def edit_invoice_num(self):
         gui.click(x=604, y=383)
@@ -120,3 +139,27 @@ class Inputter:
         self.edit_account_payable_row()
 
         # TODO: DO NOT PRESS F12 TO SAVE BECAUSE THAT WILL MAKE CHANGES TO DB
+
+if __name__ == '__main__':
+    bot = Inputter({})
+    time.sleep(3)
+    bot.click_air_import()
+    time.sleep(1)
+    bot.click_mawb_list()
+    bot.search_mawb()
+    gui.press('f7')
+    time.sleep(4)
+    bot.click_account_payable_header()
+    time.sleep(1)
+    gui.press('f2')
+    time.sleep(1)
+    bot.edit_vendor()
+    time.sleep(1)
+    bot.edit_dates()
+    time.sleep(1)
+    bot.edit_invoice_num()
+    time.sleep(1)
+    bot.edit_price('')
+    time.sleep(1)
+    bot.edit_billing_code('')
+    
